deaace011bfdc527239e4eb3f74b0e0989c637ab
wael_naamah <wael.naamah@gmail.com>
2023-02-19 11:19:42 +0400

    update navigations & screens

--- src/navigator/TabNavigator.tsx
+++ src/navigator/TabNavigator.tsx
@@ -2,8 +2,31 @@ import React, {useLayoutEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CustomersScreen from '../screens/CustomersScreen';
 import OrdersScreen from '../screens/OrdersScreen';
-import {useNavigation} from '@react-navigation/native';
+import {RouteProp, useNavigation} from '@react-navigation/native';
+import {Icon} from '@rneui/themed';
 
+type TabIconProps = {
+  route: RouteProp<any, string>;
+  focused: Boolean;
+  color?: string;
+  size?: number;
+};
+
+const TabIcon = ({route, focused}: TabIconProps) => {
+  if (route.name === 'Customers') {
+    return (
+      <Icon name="users" type="entypo" color={focused ? '#59C1CC' : 'gray'} />
+    );
+  } else if (route.name === 'Orders') {
+    return (
+      <Icon name="box" type="entypo" color={focused ? '#EB6A7c' : 'gray'} />
+    );
+  } else {
+    return (
+      <Icon name="home" type="entypo" color={focused ? '#59C1CC' : 'gray'} />
+    );
+  }
+};
 export type TabStackParamList = {
   Customers: undefined;
   Orders: undefined;
@@ -21,7 +44,14 @@ const TabNavigator = () => {
   }, [navigation]);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: '#59C1CC',
+        tabBarInactiveTintColor: 'gray',
+        tabBarIcon: ({focused, color, size}) => (
+          <TabIcon route={route} focused={focused} color={color} size={size} />
+        ),
+      })}>
       <Tab.Screen name="Customers" component={CustomersScreen} />
       <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
---
+++ src/navigator/TabNavigatorIcons.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const TabNavigatorIcons = () => {
+  return (
+    <View>
+      <Text>TabNavigatorIcons</Text>
+    </View>
+  );
+};
+
+export default TabNavigatorIcons;
