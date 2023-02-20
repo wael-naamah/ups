a5b3f20f6586fb25cf8fa1022d5d416f4a27dcaa
wael_naamah <wael.naamah@gmail.com>
2023-02-20 10:17:53 +0400

    configer stepzen & add graphql

--- App.tsx
+++ App.tsx
@@ -12,16 +12,24 @@ import utilities from './tailwind.json';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import RootNavigator from './src/navigator/RootNavigator';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://dashboard.stepzen.com/explorer?endpoint=api%2Fintended-dolphin',
+  cache: new InMemoryCache(),
+});
 
 function App(): JSX.Element {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <ApolloProvider client={client}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
